a9536ad26dc80874f7dc7d1db381dad7352bb064
ChadiElHanchi <133407188+ChadiElHanchi@users.noreply.github.com>
2025-07-20 19:27:18 +0100

    project react

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,15 +1,5 @@
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-
-import WelcomeMessage from './components/WelcomeMessage';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import UserProfile from './components/UserProfile';
-import Counter from './components/Counter';
-import UserInfo from './components/UserInfo';
-
+// src/App.jsx
+import ProfilePage from './ProfilePage';
 import UserContext from './UserContext';
 
 function App() {
@@ -20,37 +10,7 @@ function App() {
 
   return (
     <UserContext.Provider value={userData}>
-      <>
-        <div>
-          <a href="https://vite.dev" target="_blank" rel="noreferrer">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank" rel="noreferrer">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-
-        <h1>Vite + React</h1>
-
-        <WelcomeMessage />
-        <Header />
-        <MainContent />
-        <Footer />
-
-        <UserProfile 
-          name="Alice" 
-          age={25} 
-          bio="Loves hiking and photography" 
-        />
-
-        <UserInfo /> {/* ✅ Uses Context */}
-
-        <Counter />
-
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-      </>
+      <ProfilePage />
     </UserContext.Provider>
   );
 }
