fdec920a5d107a6aa4cfe9bdc96fae765d1e7c42
ChadiElHanchi <133407188+ChadiElHanchi@users.noreply.github.com>
2025-07-27 12:53:31 +0100

    Recipe fix

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -4,33 +4,35 @@ import SearchBar from './components/SearchBar';
 import FavoritesList from './components/FavoritesList';
 import RecommendationsList from './components/RecommendationsList';
 import RecipeDetails from './components/RecipeDetails';
-import { Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div style={{ padding: '2rem' }}>
-      <h1>
-        <Link to="/" style={{ textDecoration: 'none', color: 'black' }}>
-          Recipe Sharing App
-        </Link>
-      </h1>
+    <Router>
+      <div style={{ padding: '2rem' }}>
+        <h1>
+          <Link to="/" style={{ textDecoration: 'none', color: 'black' }}>
+            Recipe Sharing App
+          </Link>
+        </h1>
 
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <SearchBar />
-              <AddRecipeForm />
-              <RecipeList />
-              <FavoritesList />
-              <RecommendationsList />
-            </>
-          }
-        />
-        <Route path="/recipe/:id" element={<RecipeDetails />} />
-      </Routes>
-    </div>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <SearchBar />
+                <AddRecipeForm />
+                <RecipeList />
+                <FavoritesList />
+                <RecommendationsList />
+              </>
+            }
+          />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
