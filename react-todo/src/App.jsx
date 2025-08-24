8d45090a2781248386c098c982ee1ba65ad29eb3
ChadiElHanchi <133407188+ChadiElHanchi@users.noreply.github.com>
2025-08-24 15:21:55 +0100

    project

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import TodoList from "./components/TodoList";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
